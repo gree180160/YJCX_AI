4a6ea31d4d7e93b28373ccff4be6d488aa4a2fbc
15976988260 <1459287460@qq.com>
2024-01-30 15:05:21 +0800

    rusproflle improve

--- RUSProfile/CompanyInfoList.py
+++ RUSProfile/CompanyInfoList.py
@@ -13,14 +13,13 @@ from WRTools import ExcelHelp, WaitHelp, PathHelp, MySqlHelp_recommanded, LogHel
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
-sourceFile_dic = {'fileName': PathHelp.get_file_path(None, f'TNewBrand.xlsx.xlsx'),
+sourceFile_dic = {'fileName': PathHelp.get_file_path(None, f'TNewBrand.xlsx'),
                   'sourceSheet': 'company',
                   'colIndex': 1,
-                  'startIndex': 0,
+                  'startIndex': 5,  #Акционерное Общество Золотые Луга       ///Акционерное Общество Объединенная Компания Арматура И Насосы Ао Окан
                   'endIndex': 1313}
 
-accouts_arr = [[AccManage.rusprofile['n'], AccManage.rusprofile['p']]]
+accouts_arr = [AccManage.rusprofile['n'], AccManage.rusprofile['p']]
 default_url = 'https://www.rusprofile.ru/search-advanced'
 log_file = PathHelp.get_file_path('RUSProfile', 'DJ_product_status_log.txt')
 
@@ -67,7 +66,7 @@ def login_action():
             sure_button.click()
             time.sleep(3.0)
             time.sleep(60.0)
-        except:
+        except Exception as e:
             LogHelper.write_log(log_file, 'login error')
 
 
@@ -75,6 +74,9 @@ def login_action():
 def get_id(company_index, company_name):
     search_result = driver.find_element(By.ID, 'additional-results')
     companys = search_result.find_elements(By.CSS_SELECTOR, 'div.company-item')
+    if companys.__len__() == 0:
+        print(f'{company_name} : has no record')
+        return
     for temp_company in companys:
         # https://www.rusprofile.ru/id/1653418
         link = temp_company.find_element(By.TAG_NAME, 'a').get_attribute('href')
@@ -90,26 +92,39 @@ def goto_detail(company_index, company_name, profile_id):
     # driver.get(detail_url)
     new_tab_handle = driver.window_handles[1]
     # 切换到新打开的标签页面
-    new_tab_driver = driver.Chrome()
+    new_tab_driver = driver
     new_tab_driver.switch_to.window(new_tab_handle)
 
     WaitHelp.waitfor(True, False)
     full_name = inn = activity = rigister_date = industry_rank = adress = phone = email = website = revenue = profit = cost = ''
     try:
-        full_name = new_tab_driver.find_element(By.CSS_SELECTOR, 'company-name').text
-        inn = new_tab_driver.find_element(By.ID, 'clip_inn')
+        full_name = new_tab_driver.find_element(By.CSS_SELECTOR, 'h2.company-name').text
+        inn = new_tab_driver.find_element(By.ID, 'clip_inn').text
         activity = new_tab_driver.find_element(By.XPATH, '//*[@id="anketa"]/div[2]/div[2]/div[1]/span[2]').text
         rigister_date = new_tab_driver.find_element(By.XPATH, '//*[@id="anketa"]/div[2]/div[1]/div[1]/div[2]/dl[1]/dd').text
         industry_rank = new_tab_driver.find_element(By.XPATH, '//*[@id="anketa"]/div[2]/div[2]/div[2]/span[2]').text
         adress = new_tab_driver.find_element(By.XPATH, '//*[@id="clip_address"]').text
-        phone = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/div[1]/div/span[2]').text
-        email = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/div[2]/div/span[2]').text
-        website = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/span[2]/span[2]').text
+        try:
+            phone = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/div[1]/div/span[2]').text
+        except:
+            phone = ''
+        try:
+            email = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/div[2]/div/span[2]').text
+        except:
+            email = ''
+        try:
+            website = new_tab_driver.find_element(By.XPATH, '//*[@id="contacts-row"]/span[2]/span[2]').text
+        except:
+            website = ''
         #finalcial
-        revenue = new_tab_driver.find_element(By.XPATH, '//*[@id="ab-test-wrp"]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div[2]').text
+        revenue1 = new_tab_driver.find_element(By.XPATH,
+                                              '//*[@id="ab-test-wrp"]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div[2]/span[1]').text
+        revenue2 = new_tab_driver.find_element(By.XPATH,
+                                              '//*[@id="ab-test-wrp"]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div[2]/span[2]').text
+        revenue = revenue1 + revenue2
         profit = new_tab_driver.find_element(By.XPATH, '//*[@id="ab-test-wrp"]/div[2]/div[2]/div[1]/div/div[1]/div[2]/div[2]').text
         cost = new_tab_driver.find_element(By.XPATH, '//*[@id="ab-test-wrp"]/div[2]/div[2]/div[1]/div/div[1]/div[3]/div[2]').text
-    except e as Exception:
+    except Exception as e:
         LogHelper.write_log(log_file, f'{company_name} goto_detail error {e}')
     result = [company_name, profile_id, full_name, inn , activity , rigister_date , industry_rank , adress , phone , email , website , revenue , profit , cost ]
     ExcelHelp.add_arr_to_sheet(sourceFile_dic['fileName'], 'rusprofile', [result])
@@ -121,11 +136,12 @@ def goto_detail(company_index, company_name, profile_id):
 
 
 # 将company 那么作为搜索关键字，进行搜索
-def send_companyName_to_filter(company_name):
+def send_companyName_to_filter(company_index, company_name):
     input_area = driver.find_element(By.ID, 'advanced-search-query')
     input_area.clear()
     input_area.send_keys(company_name)
     WaitHelp.waitfor(True, False)
+    get_id(company_index, company_name)
     
 
 def main():
@@ -133,10 +149,9 @@ def main():
     all_companys = ExcelHelp.read_col_content(sourceFile_dic['fileName'], sourceFile_dic['sourceSheet'],
                                            sourceFile_dic['colIndex'])
     for (company_index, company_name) in enumerate(all_companys):
-        if company_name.__contains__('?'):
-            continue
-        elif company_index in range(sourceFile_dic['startIndex'], sourceFile_dic['endIndex']):
-            send_companyName_to_filter(company_index, company_name,)
+        if company_index in range(sourceFile_dic['startIndex'], sourceFile_dic['endIndex']):
+            print(f'{company_index} -- {company_name} :')
+            send_companyName_to_filter(company_index, company_name)
 
 
 if __name__ == "__main__":
